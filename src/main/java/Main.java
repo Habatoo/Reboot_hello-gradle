d1ac6dc89906a7a26845e441578c3b1e979b09d5
habatoo <habatoo@gmail.com>
2021-08-06 18:53:40 +0700

    master gradle feature

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,6 +5,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("hello master"));
         System.out.println(StringUtils.capitalize("hello gradle"));
 	System.out.println(StringUtils.upperCase("Hello Gradle"));
     }
