47a75146745e5bde373987541520d26b4cc092e9
habatoo <habatoo@gmail.com>
2021-08-08 11:30:32 +0700

    fix gradle hello-gradle

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(StringUtils.capitalize("hello master"));
         System.out.println(StringUtils.capitalize("hello gradle"));
-	System.out.println(StringUtils.upperCase("Hello Gradle"));
-	System.out.println(StringUtils.upperCase("Hello new feature Gradle"));
+        System.out.println(StringUtils.upperCase("Hello Gradle"));
+        System.out.println(StringUtils.upperCase("Hello new feature Gradle"));
     }
 }
